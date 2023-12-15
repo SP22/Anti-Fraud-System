2fa647c051bfb94532eeaa562ec0827ac73f150d
ihor shyshov <sp.ukr.rs@gmail.com>
2023-12-16 01:43:24 +0200

    stage 1. simple POST method

---
+++ src/antifraud/Controller.java
@@ -0,0 +1,34 @@
+package antifraud;
+
+import jakarta.validation.Valid;
+import jakarta.validation.constraints.Positive;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.validation.annotation.Validated;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/antifraud")
+@Validated
+public class Controller {
+    @PostMapping("/transaction")
+    public ResponseEntity<TransactionResponse> processTransaction(@RequestBody @Valid TransactionRequest request) {
+        long amount = request.amount();
+        String status;
+        if (amount <= 200) {
+            status = "ALLOWED";
+        } else if (amount <= 1500) {
+            status = "MANUAL_PROCESSING";
+        } else {
+            status = "PROHIBITED";
+        }
+        return new ResponseEntity<>(new TransactionResponse(status), HttpStatus.OK);
+    }
+}
+
+record TransactionRequest(@Positive long amount) {}
+
+record TransactionResponse(String result) {}
